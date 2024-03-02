df21064370ebefd11769fe3e7bb4de724b4a5331
Akshatakallur <akshatakallur10@gmail.com>
2024-03-02 13:34:42 +0530

    Mathematical_Operator

---
+++ day2/comare2nums.js
@@ -0,0 +1,24 @@
+function compareTwoNumbers(num1, num2) {
+    // Write code here
+    var num1;
+    var num2;
+    if (num1 > num2){
+        console.log('true');
+    }
+    else {
+        console.log('false');
+    }
+    if(num1 < num2){
+        console.log('true');
+    }
+    else {
+        console.log('false');
+    }
+    if(num1 == num2){
+        console.log('true');
+    }
+    else {
+        console.log('false');
+    }
+  }
+  
